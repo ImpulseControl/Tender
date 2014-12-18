ab7a39a73e19fec7d8b66bf2ebdc543266e0d464
Kevin Hummel <hummel.kevin@gmail.com>
2014-12-17 19:02:24 -0800

    Fragment Class Initiated and Stubbed

--- app/src/main/java/impulsecontrol/tender/Interval.java
+++ app/src/main/java/impulsecontrol/tender/Interval.java
@@ -1,8 +1,41 @@
 package impulsecontrol.tender;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by kelsey on 12/14/14.
  */
-public enum Interval {
-    WEEK, MONTHLY, YEARLY
+public enum Interval
+{
+    WEEK(0),
+    MONTHLY(1),
+    YEARLY(2);
+
+    //Convert to Integer and Back
+    private static final Map<Integer, Interval> lookup
+            = new HashMap<Integer, Interval>();
+
+    static {
+        for (Interval s : EnumSet.allOf(Interval.class))
+            lookup.put(s.getCode(), s);
+    }
+
+    private int code;
+
+    private Interval(int code)
+    {
+        this.code = code;
+    }
+
+    public int getCode()
+    {
+        return code;
+    }
+
+    public static Interval get(int code)
+    {
+        return lookup.get(code);
+    }
 }
--- app/src/main/java/impulsecontrol/tender/NavigationManager.java
+++ app/src/main/java/impulsecontrol/tender/NavigationManager.java
@@ -1,7 +1,94 @@
 package impulsecontrol.tender;
 
+import android.app.Fragment;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import java.util.ArrayList;
+
 /**
  * Created by hummel on 12/14/14.
  */
-public class NavigationManager {
+public class NavigationManager
+{
+    static public ArrayList<Fragment> Fragments;
+    static public Fragment currentFragment;
+
+     NavigationManager()
+     {
+         for (Interval interval :Interval.values()) {
+             Bundle args = new Bundle();
+             args.putInt("interval", interval.getCode());
+             CardListFragment newFrag = new CardListFragment();
+             newFrag.setArguments(args);
+             Fragments.add(newFrag);
+         }
+     }
+
+
+
+    public static class CardListFragment extends Fragment
+    {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            //Get Current Date/MOnth/Year
+                //TODO Make a cass do this
+            super.onCreate(savedInstanceState);
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            //Fill With Cards
+            Bundle b = getArguments();
+            int intervalValue = b.getInt("interval");
+            Interval thisInterval  = Interval.get(intervalValue);
+
+            //Get Cards from Interval Enum
+            //ArrayList<Expense> Expenses = DataBase get(Interval,Week,Month,Year)
+            //For (Size of Expenses)
+            // {
+            // Card = CreateCard(Expense[i]
+            // this.addCard(Card)
+            // }
+            return super.onCreateView(inflater, container, savedInstanceState);
+        }
+
+
+        @Override
+        public void onResume()
+        {
+            //CCheck if NEw Cards
+                //Add/Delete Cards
+            //Check IF Categories Change
+                //UPdate Categories
+
+            super.onResume();
+        }
+
+        @Override
+        public void onPause()
+        {
+            //Save Changes
+            super.onPause();
+        }
+    }
+
+
+
+    public static Fragment getFragmentFromPosition(int position)
+    {
+        return Fragments.get(position);
+    }
+
+
+
 }

15276eef730cd1c3bc022c5447951e3db60cc992
Kelsey Kerr <kerr.kelsey@gmail.com>
2014-12-20 13:30:19 -0500

    + scroll view + test cards to show scroll view + floating action button(FAB) + FAB opens dialog where user can add an expense Next Steps: - error check dialog input - display proper keyboards for dialog input - add dropdown for category field - populate dropdown with categories from the db

--- app/src/main/java/impulsecontrol/tender/MainActivity.java
+++ app/src/main/java/impulsecontrol/tender/MainActivity.java
@@ -1,18 +1,33 @@
 package impulsecontrol.tender;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Outline;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class MainActivity extends Activity {
@@ -22,6 +37,13 @@ public class MainActivity extends Activity {
     private String[] NavTitles;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+    private ImageButton fab;
+    final Context context = this;
+
+    private String description;
+    private String category;
+    private Double amount;
+    private Date date;
 
 
     @Override
@@ -35,6 +57,7 @@ public class MainActivity extends Activity {
         NavTitles = getResources().getStringArray(R.array.NavViews);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        fab = (ImageButton) findViewById(R.id.add_button);
 
         // Set the adapter for the list view
         Log.d("this is my array", "arr: " + Arrays.toString(NavTitles));
@@ -44,6 +67,78 @@ public class MainActivity extends Activity {
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
         NavigationManager nav;
 
+        // add button listener
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // get expense_dialog.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View expenseDialogView = li.inflate(R.layout.expense_dialog, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set expense_dialog.xml to alertdialog builder
+                alertDialogBuilder.setView(expenseDialogView);
+
+                final EditText descriptionResult = (EditText) expenseDialogView
+                        .findViewById(R.id.editDescription);
+
+                final EditText categoryResult = (EditText) expenseDialogView
+                        .findViewById(R.id.editCategory);
+
+                final EditText amountResult = (EditText) expenseDialogView
+                        .findViewById(R.id.editAmount);
+
+                final DatePicker dateResult = (DatePicker) expenseDialogView
+                        .findViewById(R.id.editDate);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setTitle("Add An Expense")
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // get user input and set it to results
+                                        //TODO:
+                                        // - error check dialog
+                                        // - if missing values, display message
+                                        // - save new object to database
+                                        // - create and display card
+                                        description = descriptionResult.getText().toString().trim();
+                                        category = categoryResult.getText().toString().trim();
+                                        amount = Double.parseDouble(amountResult.getText().toString().trim());
+
+                                        int day = dateResult.getDayOfMonth();
+                                        int month = dateResult.getMonth();
+                                        int year = dateResult.getYear();
+
+                                        Calendar calendar = Calendar.getInstance();
+                                        calendar.set(year, month, day);
+
+                                        date = calendar.getTime();
+
+                                        dialog.dismiss();
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+            }
+        });
 
 
     }
@@ -84,23 +179,24 @@ public class MainActivity extends Activity {
     }
 
 
-        /**
-         * Swaps fragments in the main content
-         * view
-         */
-        private void selectItem(int position) {
-            // Create a new fragment and specify t
-            Fragment fragment = new Fragment();
-            Bundle args = new Bundle();
-            //fragment.setArguments();
+    /**
+     * Swaps fragments in the main content
+     * view
+     */
+    private void selectItem(int position) {
+        // Create a new fragment and specify t
+        Fragment fragment = new Fragment();
+        Bundle args = new Bundle();
+        //fragment.setArguments();
 
-            //Insert Fragment Using Fragment Manager();
+        //Insert Fragment Using Fragment Manager();
 
-            // Highlight the selected item, update the title, and close the drawer
-            mDrawerList.setItemChecked(position, true);
-            setTitle("Title");
-            mDrawerLayout.closeDrawer(mDrawerList);
-        }
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle("Title");
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
 //
 //    @Override
 //    public void setTitle(CharSequence title) {
